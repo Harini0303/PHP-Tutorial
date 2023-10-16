1db8ac4fa3a76f4614d762f89ff2af9c8cb73cfe
HARINI <112405815+Harini0303@users.noreply.github.com>
2023-10-16 20:41:39 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        body{
+    padding:50px;
+    }
+    .container{
+    max-width: 600px;
+    margin:0 auto;
+    padding:50px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
+    .form-group{
+    margin-bottom:30px;
+    text-align:center;
+    }
+    .form-btn {
+        text-align:center;
+    }
+    .already {
+        text-align:center;
+    }
+    .h3{
+        text-align:center;
+    }
+</style>
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["submit"])) {
+           $fullName = $_POST["fullname"];
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+           $passwordRepeat = $_POST["repeat_password"];
+           
+           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+           $errors = array();
+           
+           if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+            array_push($errors,"All fields are required");
+           }
+           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($errors, "Email is not valid");
+           }
+           if (strlen($password)<8) {
+            array_push($errors,"Password must be at least 8 characters long");
+           }
+           if ($password!==$passwordRepeat) {
+            array_push($errors,"Password does not match");
+           }
+
+           include "connection.php";
+           $sql = "SELECT * FROM users WHERE email = '$email'";
+           $result = mysqli_query($connection, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Email already exists!");
+           }
+           if (count($errors)>0) {
+            foreach ($errors as  $error) {
+                echo "<div class='alert alert-danger'>$error</div>";
+            }
+           }else{
+            
+            $sql = "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)";
+            $stmt = mysqli_stmt_init($connection);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+                mysqli_stmt_bind_param($stmt, "sss", $fullName, $email, $passwordHash);
+                mysqli_stmt_execute($stmt);
+                echo "<div class='alert alert-success'>You are registered successfully.</div>";
+            }else{
+                die("Something went wrong");
+            }
+           }
+          
+
+        }
+        ?>
+
+        <h3 class="h3">Technical Event Kpriet</h3>
+        <br>
+        <form action="register.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-group">
+                <input type="emamil" class="form-control" name="email" placeholder="Email:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password:">
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+        </form>
+        <div>
+            <br>
+        <div class="already"><p>Already Registered <a href="login.php">Login Here</a></p></div>
+      </div>
+    </div>
+</body>
+</html>
